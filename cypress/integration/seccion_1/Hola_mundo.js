3dfba0a6d38d84ed9b4f012eff808c13e39f1cf9
Maria Gomez <maria_gg86@yahoo.es>
2021-12-12 18:17:37 +0100

    arrancando el primer test Hola mundo

---
+++ cypress/integration/seccion_1/Hola_mundo.js
@@ -0,0 +1,15 @@
+
+describe("Bienvenidoa al curso de Cypress sección 1", () => {
+    
+    it("mi primer test -> Hola mundo", () => {
+        cy.log("Hola mundo");
+        cy.wait(1000);
+    })
+
+    it("Segundo test -> campo name", () => {
+        cy.visit("https://demoqa.com/text-box");
+        cy.get("#userName").type("Rodrigo");
+        cy.wait(1000);
+    })
+
+})
