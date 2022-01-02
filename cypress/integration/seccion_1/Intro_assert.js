b1811a4c3eef48491614801a754e2622ae2717d1
Maria Gomez <maria_gg86@yahoo.es>
2022-01-02 18:28:11 +0100

    Introducción a los Asserts

---
+++ cypress/integration/seccion_1/Intro_assert.js
@@ -0,0 +1,16 @@
+/// <reference types="Cypress" />
+
+describe("Introducción a los asserts", () => {
+    
+    it("Demo de los asserts ", () => {
+        cy.visit("https://demoqa.com/automation-practice-form");
+        cy.title().should('eq', 'ToolsQA');
+        cy.wait(1000);
+        cy.get("#firstName").should("be.visible").type("Juan");
+        cy.wait(1000);
+        cy.get("#lastName").should("be.visible").type("Perez");
+        cy.wait(1000);
+        cy.get("#userEmail").should("be.visible").should("be.enabled").type("juan@gmail.com");
+    })
+
+})
