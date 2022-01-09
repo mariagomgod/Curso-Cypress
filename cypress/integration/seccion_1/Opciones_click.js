99f9c48fd6f5262542c48d975776e09b68afc084
Maria Gomez <maria_gg86@yahoo.es>
2022-01-09 17:15:44 +0100

    click básico

---
+++ cypress/integration/seccion_1/Opciones_click.js
@@ -0,0 +1,18 @@
+/// <reference types="Cypress" />
+
+describe("Opciones de click", () => {
+    
+    it("Click sencillo", () => {
+        cy.visit("https://opensource-demo.orangehrmlive.com/");
+        cy.title().should('eq', 'OrangeHRM');
+        cy.wait(1000);
+        cy.get("#txtUsername").should("be.visible").type("Admin");
+        cy.wait(1000);
+        cy.get("#txtPassword").should("be.visible").type("admin123");
+        cy.wait(1000);
+        cy.get("#btnLogin").should("be.visible").click();
+        cy.get("#menu_admin_viewAdminModule > b").should("be.visible").click();
+        cy.get("#menu_admin_Job").should("not.be.visible").click({force: true});
+    })
+
+})
