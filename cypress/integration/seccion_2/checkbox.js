18093eb0583cd221ecc4409644161008a51a7fe8
Maria Gomez <maria_gg86@yahoo.es>
2022-02-28 13:39:19 +0100

    checkbox function with the click option

--- cypress/integration/seccion_2/checkbox.js
+++ cypress/integration/seccion_2/checkbox.js
@@ -14,4 +14,15 @@ describe("Nueva seccion Checkbox", () => {
         cy.get("[type='checkbox']").uncheck().should("not.be.checked");
     })
 
+    it("check por selección", () => {
+        cy.visit("https://www.seleniumeasy.com/test/basic-checkbox-demo.html");
+        cy.title().should("eq", "Selenium Easy - Checkbox demo for automation using selenium");
+        cy.wait(1000);
+        //cy.get("#isAgeSelected").check().should("be.checked");
+        //cy.xpath("(//input[contains(@type, 'checkbox')])[5]").check().should("be.checked");
+        cy.get("#isAgeSelected").click();
+        cy.xpath("(//input[contains(@type, 'checkbox')])[5]").click();
+    })
+
 })
+
