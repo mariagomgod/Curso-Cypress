291198d87cc701ef84a3e279d6682c1408d465a1
Maria Gomez <maria_gg86@yahoo.es>
2022-02-28 13:20:55 +0100

    checkbox function

---
+++ cypress/integration/seccion_2/checkbox.js
@@ -0,0 +1,17 @@
+/// <reference types="Cypress" />
+
+require('cypress-plugin-tab')
+require('cypress-xpath')
+
+describe("Nueva seccion Checkbox", () => {
+    
+    it("check uno", () => {
+        cy.visit("https://www.seleniumeasy.com/test/basic-checkbox-demo.html");
+        cy.title().should("eq", "Selenium Easy - Checkbox demo for automation using selenium");
+        cy.wait(1000);
+        cy.get("[type='checkbox']").check().should("be.checked");
+        cy.wait(1000);
+        cy.get("[type='checkbox']").uncheck().should("not.be.checked");
+    })
+
+})
