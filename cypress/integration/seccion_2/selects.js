77344913e00ee0f9d00bf6459a4dafb8550a7181
Maria Gomez <maria_gg86@yahoo.es>
2022-03-06 13:51:12 +0100

    functions with selects

--- cypress/integration/seccion_2/selects.js
+++ cypress/integration/seccion_2/selects.js
@@ -4,15 +4,29 @@ require('cypress-plugin-tab')
 require('cypress-xpath')
 
 describe("Selects", () => {
-    
+    Cypress.on('uncaught:exception', (err, runnable) => {
+        // returning false here prevents Cypress from
+        // failing the test
+        return false
+    });
+
     it("selects", () => {
         cy.visit("https://web.archive.org/web/20180920020915/http://www.seleniumeasy.com/test/basic-select-dropdown-demo.html");
         cy.title().should("eq", "Selenium Easy Demo - Automate All Scenarios");
         cy.wait(1000);
-        cy.get("#select-demo").should("be.visible").select("Friday").should("have.value", "Friday");
+        cy.get("#select-demo").should("be.visible").select("Friday", {force: true}).should("have.value", "Friday");
         cy.wait(1000);
-        cy.get("#select-demo").should("be.visible").select("Saturday").should("have.value", "Saturday");
+        cy.get("#select-demo").should("be.visible").select("Saturday", {force: true}).should("have.value", "Saturday");
 
+    })
+
+    it.only("selects", () => {
+        cy.visit("https://www.google.com");
+        cy.title().should("eq", "Google");
+        cy.wait(1000);
+        cy.get("[name='q']").should("be.visible").type("Ferrari");
+        cy.wait(1000);
+        cy.get("#voiceSearchButton").should("be.visible").click();
     })
 
 })
