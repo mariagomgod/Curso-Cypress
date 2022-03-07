b7cfe8ed77632ac69f4f98a9304329a8fcd1efed
Maria Gomez <maria_gg86@yahoo.es>
2022-03-07 20:24:16 +0100

    assert text

--- cypress/integration/seccion_2/asserts.js
+++ cypress/integration/seccion_2/asserts.js
@@ -12,12 +12,27 @@ describe("Asserts", () => {
         cy.get("#block_top_menu").contains("Women").click();
     })
 
-    it("Assert find, eq", () => {
+    it.skip("Assert find, eq", () => {
         cy.visit("http://automationpractice.com/index.php");
         cy.title().should("eq", "My Store");
         cy.wait(1000);
         cy.get(".product-container").find(".product-image-container").eq(2).click();
-        
     })
+
+    it("Assert find, eq, validando que el vestido sea nuevo", () => {
+        cy.visit("http://automationpractice.com/index.php");
+        cy.title().should("eq", "My Store");
+        cy.wait(1000);
+        cy.get(".product-container").find(".product-image-container").eq(2).click();
+        cy.get("#product_condition .editable").then((e) => {
+
+            let estado = e.text();
+            
+            if(estado == "New") {
+                cy.log("El vestido es nuevo");
+            }
+        });
+    })
+
 
 })
