650c904d8463c20f9b7feed112864c2b4b67ff57
Maria Gomez <maria_gg86@yahoo.es>
2022-03-07 20:00:49 +0100

    assert find_eq

--- cypress/integration/seccion_2/asserts.js
+++ cypress/integration/seccion_2/asserts.js
@@ -5,11 +5,19 @@ require('cypress-xpath')
 
 describe("Asserts", () => {
     
-    it("asserts", () => {
+    it.skip("Assert contains", () => {
         cy.visit("http://automationpractice.com/index.php");
         cy.title().should("eq", "My Store");
         cy.wait(1000);
         cy.get("#block_top_menu").contains("Women").click();
+    })
+
+    it("Assert find, eq", () => {
+        cy.visit("http://automationpractice.com/index.php");
+        cy.title().should("eq", "My Store");
+        cy.wait(1000);
+        cy.get(".product-container").find(".product-image-container").eq(2).click();
+        
     })
 
 })
