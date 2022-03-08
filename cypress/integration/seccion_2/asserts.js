2b83bbdd6e9a95f238e55a531305dd4853e1f6bc
Maria Gomez <maria_gg86@yahoo.es>
2022-03-08 21:05:28 +0100

    assert text2

--- cypress/integration/seccion_2/asserts.js
+++ cypress/integration/seccion_2/asserts.js
@@ -19,17 +19,32 @@ describe("Asserts", () => {
         cy.get(".product-container").find(".product-image-container").eq(2).click();
     })
 
-    it("Assert find, eq, validando que el vestido sea nuevo", () => {
+    it("Assert find, eq, validando que el vestido sea nuevo y el precio", () => {
         cy.visit("http://automationpractice.com/index.php");
         cy.title().should("eq", "My Store");
         cy.wait(1000);
-        cy.get(".product-container").find(".product-image-container").eq(2).click();
+        cy.get(".product-container").find(".product-image-container").eq(3).click();
         cy.get("#product_condition .editable").then((e) => {
-
             let estado = e.text();
             
-            if(estado == "New") {
+            if (estado == "New") {
                 cy.log("El vestido es nuevo");
+            }
+        });
+
+        cy.get("#our_price_display").then((e) => {
+            let precio = e.text();
+            precio = precio.slice(1, 3);
+            cy.log(precio);
+
+            if (precio > 30) {
+                cy.log("El vestido no está dentro de nuestro presupuesto. No lo podemos comprar");
+                cy.xpath("//*[@id='columns']/div[1]/a[2]").click();
+                cy.wait(1000);
+            } else {
+                cy.log("El vestido está dentro de nuestro presupuesto. Continua con la compra");
+                cy.get("#add_to_cart").click();
+                cy.wait(1000);
             }
         });
     })
