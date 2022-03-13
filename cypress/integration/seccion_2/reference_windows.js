e38f4989de1762dff992ab1f6ccc8fb93b91c08a
Maria Gomez <maria_gg86@yahoo.es>
2022-03-13 15:25:03 +0100

    windows validando URL's

--- cypress/integration/seccion_2/reference_windows.js
+++ cypress/integration/seccion_2/reference_windows.js
@@ -5,11 +5,24 @@ require('cypress-xpath')
 
 describe("Referencias a windows", () => {
     
-    it("Windows propiedad charset", () => {
+    it.skip("Windows propiedad charset", () => {
         cy.visit("https://testsheepnz.github.io/random-number.html");
         cy.title().should("eq", "The Number Game");
         cy.wait(1000);
         cy.document().should("have.property", "charset").and("eq", "UTF-8");
+    })
+
+    it("Windows validar una URL", () => {
+        cy.visit("https://testsheepnz.github.io/random-number.html");
+        cy.title().should("eq", "The Number Game");
+        cy.wait(1000);
+
+        // con include no hace falta que pongamos toda la url para validar, sino 
+        // solamente un fragmento
+        cy.url().should("include", "random-number.html");
+
+        // con eq sí tenemos que validar la url al completo 
+        cy.url().should("eq", "https://testsheepnz.github.io/random-number.html");
     })
 
 })
