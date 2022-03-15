a0e729667ddbc88c360ebf8dbfa0d6b47acd5a79
Maria Gomez <maria_gg86@yahoo.es>
2022-03-15 20:32:34 +0100

    Fecha tres

--- cypress/integration/seccion_3/fechas.js
+++ cypress/integration/seccion_3/fechas.js
@@ -20,12 +20,19 @@ describe("Campos de tipo fecha", () => {
         cy.get("#sandbox-container1 > .input-group > .form-control").should("be.visible").type("09/10/2021");
     })
     
-    it("Fecha dos", () => {
+    it.skip("Fecha dos", () => {
         cy.visit("https://web.archive.org/web/20180920012603/http://www.seleniumeasy.com/test/bootstrap-date-picker-demo.html");
         cy.title().should('eq', 'Selenium Easy - Best Demo website for Bootstrap Date picker');
         cy.wait(1000);
         cy.get("[placeholder='Start date']").should("be.visible").type("09/10/2021 {esc}");
         cy.wait(1000);
         cy.get("[placeholder='End date']").should("be.visible").type("10/11/2021 {esc}");
+    })
+
+    it("Fecha tres", () => {
+        cy.visit("https://web.archive.org/web/20180920012603/http://www.seleniumeasy.com/test/bootstrap-date-picker-demo.html");
+        cy.title().should('eq', 'Selenium Easy - Best Demo website for Bootstrap Date picker');
+        cy.wait(1000);
+        cy.get("[placeholder='Start date']").should("be.visible").type("09/10/2021 {esc}").tab().type("10/11/2021 {esc}");
     })
 })
