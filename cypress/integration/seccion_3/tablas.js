e9f3856b5cb2a35c55ec09497c1d125249793ee7
Maria Gomez <maria_gg86@yahoo.es>
2022-03-23 19:03:24 +0100

    Reto tablas con for

--- cypress/integration/seccion_3/tablas.js
+++ cypress/integration/seccion_3/tablas.js
@@ -88,7 +88,7 @@ describe("Elementos de una tabla", () => {
         cy.get("[type='button']").parent().should("have.class", "btn-group");
     })
 
-    it("Reto de las tablas", () => {
+    it.skip("Reto de las tablas", () => {
 
         cy.visit("https://web.archive.org/web/20180920012603/http://www.seleniumeasy.com/test/table-records-filter-demo.html");
         cy.title().should('eq', 'Selenium Easy - Table Data Filter Demo');
@@ -96,5 +96,20 @@ describe("Elementos de una tabla", () => {
         cy.get("[type='button']").eq(4).should("contain", "All").click({force:true});
         cy.wait(1500);
         cy.get("[type='checkbox']").check({force:true});
+    })
+
+    it("Reto de las tablas con for", () => {
+
+        cy.visit("https://web.archive.org/web/20180920012603/http://www.seleniumeasy.com/test/table-records-filter-demo.html");
+        cy.title().should('eq', 'Selenium Easy - Table Data Filter Demo');
+        cy.wait(1500);
+
+        for (let x = 1; x <= 4; x++) {
+            cy.get("[type='button']").eq(x).click({force:true});
+            cy.wait(1500);
+            cy.get("[type='checkbox']").check({force:true});
+            cy.wait(1500);
+        }
+        
     })
 })
