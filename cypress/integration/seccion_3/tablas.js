1b4996762ab6006496d44fa161846d37594203fd
Maria Gomez <maria_gg86@yahoo.es>
2022-03-23 16:04:03 +0100

    Tablas elemento find

--- cypress/integration/seccion_3/tablas.js
+++ cypress/integration/seccion_3/tablas.js
@@ -36,7 +36,7 @@ describe("Elementos de una tabla", () => {
         cy.get("[type='button']").eq(4).should("contain", "All").click({force:true});
     })
 
-    it("Elemento Filter", () => {
+    it.skip("Elemento Filter", () => {
 
         cy.visit("https://web.archive.org/web/20180920012603/http://www.seleniumeasy.com/test/table-records-filter-demo.html");
         cy.title().should('eq', 'Selenium Easy - Table Data Filter Demo');
@@ -46,5 +46,16 @@ describe("Elementos de una tabla", () => {
         cy.wait(1500);
         cy.get("[type='button']").filter(".btn-success").click();
         cy.get("[type='button']").filter(".btn-warning").should("contain", "Orange").click();
+    })
+
+    it("Elemento Find", () => {
+
+        cy.visit("https://web.archive.org/web/20180920012603/http://www.seleniumeasy.com/test/table-records-filter-demo.html");
+        cy.title().should('eq', 'Selenium Easy - Table Data Filter Demo');
+        cy.wait(1500);
+        cy.get(".btn-group").find(".btn-danger").should("contain", "Red").click();
+        cy.wait(1500);
+        cy.get(".btn-group").contains("Orange").click();
+       
     })
 })
