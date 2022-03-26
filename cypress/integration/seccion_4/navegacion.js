d5c59565a68373c775e8ec4afb548c191c33e2e0
Maria Gomez <maria_gg86@yahoo.es>
2022-03-26 19:44:25 +0100

    Navegación con Reload

--- cypress/integration/seccion_4/navegacion.js
+++ cypress/integration/seccion_4/navegacion.js
@@ -7,7 +7,7 @@ require('@4tw/cypress-drag-drop')
 
 
 describe('Navegación entre las páginas', () => { 
-    it('Back, forward', () => {
+    it.skip('Back, forward', () => {
         let time = 1000;
         cy.visit('https://demoqa.com/');
         cy.title().should('eq', 'ToolsQA');
@@ -20,5 +20,23 @@ describe('Navegación entre las páginas', () => {
         cy.go("back");
         cy.go("forward");
         cy.go("forward");
+    })
+
+    it('Reload', () => {
+        let time = 1000;
+        cy.visit('https://demoqa.com/');
+        cy.title().should('eq', 'ToolsQA');
+        cy.wait(time);
+        cy.get(':nth-child(1) > :nth-child(1) > .avatar > svg').should('be.visible').click({force:true});
+        cy.wait(time);
+        cy.get(':nth-child(1) > .element-list > .menu-list > #item-0 > .text').should('be.visible').click({force:true});
+        cy.wait(time);
+        cy.get('#userName').should('be.visible').type('Juan');
+        cy.wait(time);
+        cy.get('#userEmail').should('be.visible').type('juan@gmail.com');
+        cy.wait(time);
+        cy.reload();
+        cy.wait(time);
+        cy.go("back");
     })
 })
