6296ed07d70063eab773674d56dfad96df08f8ee
Maria Gomez <maria_gg86@yahoo.es>
2022-04-02 21:17:21 +0200

    Carga por archivo tipo json

--- cypress/integration/fixture/fixture1.js
+++ cypress/integration/fixture/fixture1.js
@@ -11,13 +11,22 @@ describe('Carga por Fixture', () => {
     before(function() {
         cy.fixture('datos2').then(function(data) {
             globalThis.data = data;
-        });
+        })
     })
 
     it('test uno cargando desde json', () => {
         cy.visit('https://demoqa.com/text-box');
         cy.title().should('eq', 'ToolsQA');
         cy.wait(1000);
-        
+        cy.get('#userName').should('be.visible').type(data.nombre);
+        cy.wait(1000);
+        cy.get('#userEmail').should('be.visible').type(data.email);
+        cy.wait(1000);
+        cy.get('#currentAddress').should('be.visible').type(data.dir1);
+        cy.wait(1000);
+        cy.get('#permanentAddress').should('be.visible').type(data.dir2);
+        cy.wait(1000);
+        cy.get('#submit').should('be.visible').click({force:true});
+        cy.wait(1000);
     })
 })
