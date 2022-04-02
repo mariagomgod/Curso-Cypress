f50521468ebc6f62891316849aeff68940af5eb9
Maria Gomez <maria_gg86@yahoo.es>
2022-04-02 21:25:33 +0200

    Carga por alias con json

--- cypress/integration/fixture/fixture1.js
+++ cypress/integration/fixture/fixture1.js
@@ -8,25 +8,32 @@ require('@4tw/cypress-drag-drop')
 
 describe('Carga por Fixture', () => { 
 
-    before(function() {
+/*     before(function() {
         cy.fixture('datos2').then(function(data) {
             globalThis.data = data;
         })
+    }) */
+
+    before(function() {
+        cy.fixture("datos2").as("datos_json"); // el alias
     })
 
     it('test uno cargando desde json', () => {
         cy.visit('https://demoqa.com/text-box');
         cy.title().should('eq', 'ToolsQA');
         cy.wait(1000);
-        cy.get('#userName').should('be.visible').type(data.nombre);
-        cy.wait(1000);
-        cy.get('#userEmail').should('be.visible').type(data.email);
-        cy.wait(1000);
-        cy.get('#currentAddress').should('be.visible').type(data.dir1);
-        cy.wait(1000);
-        cy.get('#permanentAddress').should('be.visible').type(data.dir2);
-        cy.wait(1000);
-        cy.get('#submit').should('be.visible').click({force:true});
-        cy.wait(1000);
+
+        cy.get("@datos_json").then((data) => {
+            cy.get('#userName').should('be.visible').type(data.nombre);
+            cy.wait(1000);
+            cy.get('#userEmail').should('be.visible').type(data.email);
+            cy.wait(1000);
+            cy.get('#currentAddress').should('be.visible').type(data.dir1);
+            cy.wait(1000);
+            cy.get('#permanentAddress').should('be.visible').type(data.dir2);
+            cy.wait(1000);
+            cy.get('#submit').should('be.visible').click({force:true});
+            cy.wait(1000);
+        })
     })
 })
