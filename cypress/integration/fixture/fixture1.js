232b33dcafa8056eb3d38a37fa6f14f74c0311f3
Maria Gomez <maria_gg86@yahoo.es>
2022-04-03 14:10:47 +0200

    Reto fixture

--- cypress/integration/fixture/fixture1.js
+++ cypress/integration/fixture/fixture1.js
@@ -14,26 +14,39 @@ describe('Carga por Fixture', () => {
         })
     }) */
 
-    before(function() {
+    /* before(function() {
         cy.fixture("datos2").as("datos_json"); // el alias
-    })
+    }) */
 
     it('test uno cargando desde json', () => {
-        cy.visit('https://demoqa.com/text-box');
+        /* cy.visit('https://demoqa.com/text-box');
         cy.title().should('eq', 'ToolsQA');
-        cy.wait(1000);
+        cy.wait(1000); */
 
-        cy.get("@datos_json").then((data) => {
-            cy.get('#userName').should('be.visible').type(data.nombre);
-            cy.wait(1000);
-            cy.get('#userEmail').should('be.visible').type(data.email);
-            cy.wait(1000);
-            cy.get('#currentAddress').should('be.visible').type(data.dir1);
-            cy.wait(1000);
-            cy.get('#permanentAddress').should('be.visible').type(data.dir2);
-            cy.wait(1000);
-            cy.get('#submit').should('be.visible').click({force:true});
-            cy.wait(1000);
+        cy.fixture("datos2").then(testdata => {
+            testdata.forEach(data => {
+                const d_nombre = data.nombre;
+                const d_email = data.email;
+                const d_dir1 = data.dir1;
+                const d_dir2 = data.dir2;
+
+                cy.visit('https://demoqa.com/text-box');
+                cy.title().should('eq', 'ToolsQA');
+                cy.wait(1000);
+
+                cy.get('#userName').should('be.visible').type(d_nombre);
+                cy.wait(1000);
+                cy.get('#userEmail').should('be.visible').type(d_email);
+                cy.wait(1000);
+                cy.get('#currentAddress').should('be.visible').type(d_dir1);
+                cy.wait(1000);
+                cy.get('#permanentAddress').should('be.visible').type(d_dir2);
+                cy.wait(1000);
+                cy.get('#submit').should('be.visible').click({force:true});
+                cy.wait(1000);
+            })
         })
+           
     })
+
 })
