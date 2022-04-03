398167ef14ab5712f694a056de52c4dec2a20aac
Maria Gomez <maria_gg86@yahoo.es>
2022-04-03 16:24:11 +0200

    Pasando parámetros a nuestras definiciones

--- cypress/integration/cucumber2.feature
+++ cypress/integration/cucumber2.feature
@@ -4,7 +4,9 @@ Feature: Demo de cucumber dos
 
     Scenario: Demo de cucumber dos
     Given Abrir el navegador principal
-    When Cargando el nombre
-    When Cargando el email 
-    And Cargando la dirección
+    When Cargando el nombre María
+    When Cargando el email maria@gmail.com
+    And Cargando la dirección Demo_Dirección_uno
+    And Cargando la dirección dos Demo_Dirección_dos
+    And Click en botón
     Then Validar el nombre de la página
--- cypress/integration/cucumber2/definition_spec.js
+++ cypress/integration/cucumber2/definition_spec.js
@@ -2,18 +2,28 @@ Given('Abrir el navegador principal', () => {
     cy.visit("https://demoqa.com/text-box");
 })
 
-When('Cargando el nombre', () => {
-    cy.get('#userName').should('be.visible').type('María');
+When('Cargando el nombre {word}', (name) => {
+    cy.get('#userName').should('be.visible').type(name);
     cy.wait(1000);
 })
 
-When('Cargando el email', () => {
-    cy.get('#userEmail').should('be.visible').type('maria@gmail.com');
+When('Cargando el email {word}', (email) => {
+    cy.get('#userEmail').should('be.visible').type(email);
     cy.wait(500);
 })
 
-And('Cargando la dirección', () => {
-    cy.get('#currentAddress').should('be.visible').type('Dirección uno');
+And('Cargando la dirección {word}', (dir1) => {
+    cy.get('#currentAddress').should('be.visible').type(dir1);
+    cy.wait(500);
+})
+
+And('Cargando la dirección dos {word}', (dir2) => {
+    cy.get('#permanentAddress').should('be.visible').type(dir2);
+    cy.wait(500);
+})
+
+And('Click en botón', () => {
+    cy.get('#submit').should('be.visible').click({force:true});
     cy.wait(500);
 })
 
