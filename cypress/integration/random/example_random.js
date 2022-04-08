94de22dcc3372753667a5aaa07728ec577bcf53b
Maria Gomez <maria_gg86@yahoo.es>
2022-04-08 20:00:04 +0200

    Carga por randoms ejemplo modificado

--- cypress/integration/random/example_random.js
+++ cypress/integration/random/example_random.js
@@ -9,7 +9,7 @@ require('@4tw/cypress-drag-drop')
 
 describe('Métodos de carga por Randoms', () => { 
 
-    let numero_pruebas = 10;
+    let numero_pruebas = 5;
 
     it("mi primer test -> Hola mundo", () => {
         cy.log("Hola mundo");
@@ -19,15 +19,15 @@ describe('Métodos de carga por Randoms', () => {
     it("Test random", () => {
         for (let num = 1; num <= numero_pruebas; num++) {
 
-            let estado_arr = ["1: 1", "3: 13", "5: 32"];
+            let estado_arr = ["Rodrigo", "Rosa", "Carlos"];
             let random_estado = estado_arr[Math.floor(Math.random() * estado_arr.length)];
             let cantidad = Math.floor(Math.random() * 3000);
 
             cy.visit('https://demoqa.com/text-box');
             cy.title().should('eq', 'ToolsQA');
-            cy.xpath('//input[contains(@id,"userName")]').should('be.visible').type('Name' + cantidad);
+            cy.xpath('//input[contains(@id,"userName")]').should('be.visible').type('Name ' + random_estado);
             cy.wait(500);
-            cy.xpath('//input[contains(@id,"userEmail")]').should('be.visible').type('Name' + cantidad);
+            cy.xpath('//input[contains(@id,"userEmail")]').should('be.visible').type('email' + cantidad + 'gmail.com');
             cy.wait(500);
         }
     })
